355d87b8c1079e0005f1c08ab25f248261183cfa
Bruno Faviero <bruno@kensho.com>
2014-11-19 13:26:07 -0500

    Clean up

--- oauth_classes.py
+++ oauth_classes.py
@@ -1,22 +1,17 @@
 class Client():
-	def __init__(self, client_id, secret, _redirect_uris):
-	    self.client_id = "example_app"
-	    self.client_secret = "abc123"
-	    _redirect_uris = ["secret_callback"]
+    def __init__(self, client_id, secret, _redirect_uris):
+        self.client_id = "example_app"
+        self.client_secret = "abc123"
+        _redirect_uris = ["secret_callback"]
 
-	def save(self, mogno):
-		mongo.clients.insert({
-			'client_id': client_id,
-			'secret':secret,
-			'_redirect_uris': _redirect_uris
-		})
+    def save(self, mogno):
+        mongo.clients.insert({
+            'client_id': client_id,
+            'secret':secret,
+            '_redirect_uris': _redirect_uris
+        })
 
-		return self
-
-	@staticmethod
-	def get(mongo, client_id):
-		client =  mongo.clients.find_one({"client_id": client_id})
-    	return Client(client**)
+        return self
 
     @property
     def client_type(self):
@@ -34,25 +29,30 @@ class Client():
     def default_redirect_uri(self):
         return self.redirect_uris[0]
 
+    @staticmethod
+    def get(mongo, client_id):
+        client =  mongo.clients.find_one({"client_id": client_id})
+        return Client(**client)
+
 class Grant():
-	def __init__(self, user_id, client_id, code, redirect_uri, expires):
-   		self.user_id = user_id
-   		self.client_id = client_id
-   		self.code = code
-   		self.redirect_uri = redirect_uri
-   		self.expires = expires
+    def __init__(self, user_id, client_id, code, redirect_uri, expires):
+        self.user_id = user_id
+        self.client_id = client_id
+        self.code = code
+        self.redirect_uri = redirect_uri
+        self.expires = expires
 
-   	def save(self, mongo):
-   		mongo.clients.insert({
-   			'_id': client_id,
-   			'secret':secret,
-   			'_redirect_uris':  _redirect_uris
-   		})
+    def save(self, mongo):
+        mongo.clients.insert({
+            '_id': client_id,
+            'secret':secret,
+            '_redirect_uris':  _redirect_uris
+        })
 
-	@staticmethod
-	def get(mogno, client_id):
-		grant =  mongo.grants.find_one({'client_id':client_id, 'code':code})
-    	return Grant(grant**)
+    @staticmethod
+    def get(mogno, client_id):
+        grant =  mongo.grants.find_one({'client_id':client_id, 'code':code})
+        return Grant(**grant)
 
     def delete(self):
         mongo.grants.remove({'client_id':client_id, 'code':code})
@@ -60,31 +60,31 @@ class Grant():
 
 
 class Token(db.Model):
-	def __init__(self, access_token, refresh_token, token_type, expires, client_id, user_id):
-		self.access_token = access_token
-		self.refresh_token = refresh_token
-		self.token_type = token_type
-		self.expires = expires
-		self.client_id = client_id
-		self.user_id = user_id
+    def __init__(self, access_token, refresh_token, token_type, expires, client_id, user_id):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.token_type = token_type
+        self.expires = expires
+        self.client_id = client_id
+        self.user_id = user_id
 
-	def save(mongo):
-		mongo.tokens.insert({
-				'access_token' : self.access_token,
-				'refresh_token' : self.refresh_token,
-				'token_type'	: self.token_type
-				'expires'	: self.expires,
-				'client_id'	: self.client_id,
-				'user_id'	: self.user_id
-			})
+    def save(mongo):
+        mongo.tokens.insert({
+                'access_token' : self.access_token,
+                'refresh_token' : self.refresh_token,
+                'token_type'    : self.token_type,
+                'expires'   : self.expires,
+                'client_id' : self.client_id,
+                'user_id'   : self.user_id
+            })
 
-	@staticmethod
-	def find(mongo, query):
-		return mogno.tokens.find(query)
+    @staticmethod
+    def find(mongo, query):
+        return mogno.tokens.find(query)
 
-	@staticmethod
-	def get(mongo, token):
-		mongo.tokens.find_one({"access_token": token})
+    @staticmethod
+    def get(mongo, token):
+        mongo.tokens.find_one({"access_token": token})
 
     def delete(self):
         mongo.remove({'access_token': self.access_token})
